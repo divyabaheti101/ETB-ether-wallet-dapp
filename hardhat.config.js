dbe7b868bc9900a8a45bfaed9d6d07724de7e5d4
Divya <divyabaheti101@gmail.com>
2025-01-11 14:33:45 +0530

    creating tasks in hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    const balance = await account.getBalance()
+    console.log(account.address, ': ', balance);
+  }
+});
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.formatEther(balance), "ETH");
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
