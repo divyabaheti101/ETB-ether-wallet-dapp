b800eebfdf2b5292e160e0c88dd4f1ad246cab95
Divya <divyabaheti101@gmail.com>
2025-01-11 13:56:27 +0530

    hardhat deploy and run on local node

--- ignition/modules/EtherWallet.js
+++ ignition/modules/EtherWallet.js
@@ -3,11 +3,9 @@
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
-
 module.exports = buildModule("EtherWalletModule", (m) => {
   const etherWallet = m.contract("EtherWallet");
-
+console.log('Deployed to ', etherWallet);
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
   return { etherWallet };
 });
