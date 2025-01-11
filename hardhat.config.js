5ed31b49981add08fc081f28c46a00570baad5f0
Divya <divyabaheti101@gmail.com>
2025-01-11 10:49:22 +0530

    edit hardhat config, and remove default files

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,18 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  paths: {
+    sources: './contracts',
+    artifacts: './src/artifacts'
+  },
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat:{
+      chainId: 1337
+    },
+    goerli: {
+      url: 'https://sepolia.drpc.org'
+      //accounts
+    }
+  }
 };
